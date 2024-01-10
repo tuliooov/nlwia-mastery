3cab19141843d7af1a9ce2736510c3ad9afb2892
Marco Tulio <marcotuliovaleriano2014@gmail.com>
2024-01-10 18:56:53 -0300

    fix fastify

--- api/src/server.ts
+++ api/src/server.ts
@@ -5,18 +5,22 @@ import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
 import { generateAiCompletionRoute } from "./routes/generate-ia-completion";
 
-const app = fastify()
+const port = process.env.PORT || 3000;
+const host = "RENDER" in process.env ? `0.0.0.0` : `localhost`;
+
+const app = fastify();
 
 app.register(fastifyCors, {
-    origin: "*",
-})
-app.register(getAllPromptsRoute)
-app.register(uploadVideoRoute)
-app.register(createTranscriptionRoute)
-app.register(generateAiCompletionRoute)
+  origin: "*",
+});
+app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
+app.register(generateAiCompletionRoute);
 
-app.listen({
-    port: 3333,
-}).then(() => {
-    console.log('Http server running!')
-})
+app.listen({ port: 3000, host: "0.0.0.0" }, (err, address) => {
+  if (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+});
